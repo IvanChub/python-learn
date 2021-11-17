4626e15e9403ce9e7e9904c552cece40553251a3
IvanChub <70125930+IvanChub@users.noreply.github.com>
2021-11-17 01:48:48 -0600

    creacion modulo administrador y estudiante

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectPath()
+    {
+        if(Auth::user()->tipo_usuario)
+        {
+            return '/amdmin/panel';
+        }
+        return '/home';
     }
 }
